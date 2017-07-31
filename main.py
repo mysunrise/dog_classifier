9db8035ae4c590bbd1f131d2af69373e0cff7fd3
mysunrise <10110907@sjtu.edu.cn>
2017-07-31 23:49:27 +0800

    add args

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from __future__ import print_function
 from __future__ import division
 
+import argparse
 import time
+import os
 import shutil
+
+from models import *
 import torch
 import torchvision
 import torch.nn as nn
@@ -13,113 +17,126 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.autograd import Variable
 
-traindir = './data/train'
-valdir = './data/val'
-normalizer = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                  std=[0.229, 0.224, 0.225])
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet152',
+                    help='model architecture: vgg19, resnet152')
+parser.add_argument('--num_classes', default=100, type=int, metavar='N',
+                    help='number of total class')
+parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--lr', '--learning_rate', default=0.01, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight_decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--print_freq', '-p', default=20, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--save_freq', '-s', default=10, type=int,
+                    metavar='N', help='save frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--save_dir', default='.', type=str, metavar='PATH',
+                    help='path to save the models')
 
-train_loader = torch.utils.data.DataLoader(
-    datasets.ImageFolder(traindir, transforms.Compose([
-        transforms.RandomSizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        normalizer
-    ])),
-    batch_size=64, shuffle=True)
-
-val_loader = torch.utils.data.DataLoader(
-    datasets.ImageFolder(valdir, transforms.Compose([
-        transforms.Scale(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        normalizer
-    ])),
-    batch_size=64, shuffle=False)
-
-use_gpu = torch.cuda.is_available()
-print_frequency = 20
-
-original_model = models.vgg19(pretrained=True)
-'''
-for param in original_model.parameters():
-    print(param)
-
-for name, param in original_model.named_parameters():
-    print(name, param.size())
-
-for name, module in original_model.named_children():
-    print(name, module)
-for name, param in original_model.classifier.named_parameters():
-    print(name, param.data)
-
-for name, module in original_model.named_modules():
-    print(name, module)
-'''
-
-
-class MyVgg19(nn.Module):
-    def __init__(self, pretrained_model, num_classes):
-        super(MyVgg19, self).__init__()
-        self.features = pretrained_model.features
-        mod = list(pretrained_model.classifier.children())[:-1]
-        mod.append(nn.Linear(4096, num_classes))
-        self.classifier = nn.Sequential(*mod)
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-
-# print(*list(original_model.classifier.children())[:-1])
-# print(list(original_model.classifier.children())[:-1])
-
-num_classes = 100
-model = MyVgg19(original_model, num_classes)
-criterion = nn.CrossEntropyLoss()
-if use_gpu:
-    model.cuda()
-    criterion.cuda()
-# print(list(list(model.classifier.children())[0].parameters()))
-# print(list(list(model.classifier.children())[6].named_parameters()))
-# print(list(original_model.classifier.children())[6])
-
-'''
-for name, param in model.named_parameters():
-    if name not in list(list(model.classifier.children())[6].named_parameters()):
-        print(name)
-        param.requires_grad = False
-'''
-'''
-for name, param in list(model.classifier.children())[6].named_parameters():
-    print(name, param.size())
-for name, param in model.named_parameters():
-    print(name)
-for name, param in model.classifier.named_parameters():
-    print(name, param.size())
-'''
-
-ignored_params = list(map(id, list(model.classifier.children())[6].parameters()))
-base_params = filter(lambda x: id(x) not in ignored_params, model.parameters())
-for param in base_params:
-    param.requires_grad = False
-
-fc_lr = 0.01
-optimizer = optim.SGD(list(model.classifier.children())[6].parameters(), lr=fc_lr, momentum=0.9, weight_decay=5e-4)
 best_prec1 = 0
-nums_epoch = 200
 
 
 def main():
-    global best_prec1
-    for epoch in xrange(1, nums_epoch + 1):
+    global best_prec1, args, use_gpu
+    args = parser.parse_args()
+    use_gpu = torch.cuda.is_available()
+
+    # load data
+    traindir = './data/new_train'
+    valdir = './data/new_test'
+    normalizer = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                      std=[0.229, 0.224, 0.225])
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(traindir, transforms.Compose([
+            transforms.RandomSizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalizer
+        ])),
+        batch_size=32, shuffle=True)
+
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Scale(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalizer
+        ])),
+        batch_size=16, shuffle=False)
+
+    # create model
+    if args.arch == 'vgg19':
+        vgg19 = models.vgg19(pretrained=True)
+        model = Vgg19Ft(vgg19, args.num_classes)
+        ignored_params = list(map(id, list(model.classifier.children())[6].parameters()))
+        base_params = filter(lambda x: id(x) not in ignored_params, model.parameters())
+
+        optimizer = optim.SGD([{'params': base_params},
+                               {'params': list(model.classifier.children())[6].parameters(), 'lr': args.lr}],
+                              args.lr * 0.1, momentum=args.momentum, weight_decay=args.weight_decay)
+    else:
+        if args.arch == 'resnet152':
+            model = models.resnet152(pretrained=True)
+            model.fc = nn.Linear(model.fc.in_features, args.num_classes)
+            ignored_params = list(map(id, model.fc.parameters()))
+            base_params = filter(lambda x: id(x) not in ignored_params, model.parameters())
+            optimizer = optim.SGD([{'params': base_params},
+                                   {'params': model.fc.parameters(), 'lr': args.lr}],
+                                  args.lr * 0.1, momentum=args.momentum, weight_decay=args.weight_decay)
+        else:
+            print('please choose reasonable model architeture')
+            return
+
+    criterion = nn.CrossEntropyLoss()
+
+    if use_gpu:
+        model.cuda()
+        criterion.cuda()
+
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("Loading checkpoint '{}'".format(resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['model'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("Loaded checkpoint '{}' (epoch {})".format(args.resume, args.start_epoch))
+        else:
+            print("no checkpoint found at '{}'".format(args.resume))
+
+    for epoch in xrange(args.start_epoch + 1, args.epochs + 1):
         train_model(model, train_loader, criterion, optimizer, exp_lr_scheduler, epoch)
         prec1 = test_model(model, val_loader, criterion)
         is_best = prec1 > best_prec1
         best_prec1 = max(prec1, best_prec1)
-        save_checkpoint(model, epoch, is_best)
+        if epoch % args.save_freq == 0:
+            save_checkpoint({
+                'epoch': epoch,
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'best_prec1': best_prec1
+            }, epoch, False)
+        if is_best:
+            save_checkpoint({
+                'epoch': epoch,
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'best_prec1': best_prec1
+            }, epoch, is_best)
+            print('best prec1: {}'.format(best_prec1))
 
 
+'''
 def exp_lr_scheduler(optimizer, epoch, init_lr=0.01, lr_decay_epoch=40):
     lr = init_lr * (0.1 ** (epoch // lr_decay_epoch))
     if epoch % lr_decay_epoch == 0:
@@ -127,6 +144,17 @@ def exp_lr_scheduler(optimizer, epoch, init_lr=0.01, lr_decay_epoch=40):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
+    return optimizer
+'''
+
+
+def exp_lr_scheduler(optimizer, epoch, lr_decay_epoch=30):
+    i = 0
+    for param_group in optimizer.param_groups:
+        if epoch % lr_decay_epoch == 0:
+            param_group['lr'] = param_group['lr'] * 0.1
+            print('learning rate for param_group {} is set to {}'.format(i, param_group['lr']))
+        i += 1
     return optimizer
 
 
@@ -157,7 +185,7 @@ def train_model(model, train_loader, criterion, optimizer, lr_scheduler, epoch):
         batch_time.update(time.time() - since)
         since = time.time()
 
-        if idx % print_frequency == 0:
+        if idx % args.print_freq == 0:
             print('Epoch: [{0}] [{1}/{2}]\t'
                   'Time {batch_time.val: .3f} {batch_time.avg: .3f}\t'
                   'Loss {loss.val: .4f} {loss.avg: .4f}\t'
@@ -187,7 +215,7 @@ def test_model(model, val_loader, criterion):
         batch_time.update(time.time() - since)
         since = time.time()
 
-        if idx % print_frequency == 0:
+        if idx % args.print_freq == 0:
             print('Test: [{0}/{1}]\t'
                   'Time {batch_time.val: .3f} {batch_time.avg: .3f}\t'
                   'Loss {loss.val: .4f} {loss.avg: .4f}\t'
@@ -217,12 +245,13 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def save_checkpoint(model, epoch, is_best):
-    model_out_path = './result/model_epoch_{}.pth'.format(epoch)
-    torch.save(model, model_out_path)
-    print('checkpoint saved to {}'.format(model_out_path))
+def save_checkpoint(state, epoch, is_best):
     if is_best:
-        shutil.copy(model_out_path, './result/model_best.pth')
+        torch.save(state, os.path.join(args.save_dir, 'model_best.pth'))
+        return
+    model_out_path = os.path.join(args.save_dir, 'model_epoch_{}.pth'.format(epoch))
+    torch.save(state, model_out_path)
+    print('checkpoint saved to {}'.format(model_out_path))
 
 
 if __name__ == '__main__':
