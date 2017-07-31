b38d275fd030606c535f4d29c22d4e9f2977d6a3
mysunrise <10110907@sjtu.edu.cn>
2017-07-31 15:03:49 +0800

    define class Vgg19Ft

--- models/vgg.py
+++ models/vgg.py
@@ -1,29 +1,16 @@
-from __future__ import print_function
-import torch
-import torchvision
-import torchvision.models as models
+import torch.nn as nn
 
-vgg19 = models.vgg19(pretrained=True)
-if torch.cuda.is_available():
-    vgg19 = vgg19.cuda()
-# print(vgg19)
-for name, module in vgg19.named_children():
-    print(name)
-    if name in ['classifier']:
-        for newname in vgg19.classifier.named_children():
-            print(newname)
-# vgg19.classifier[6]=torch.nn.Linear(4096,100)
-print(vgg19.classifier[6])
-'''
-for model_name in models.__dict__:
-    print(model_name)
 
-for idx, m in enumerate(vgg19.named_modules()):
-    print(idx, '->', m)
-for name, param in vgg19.named_parameters():
-    print(name, param.size())
-for idx, key in enumerate(vgg19.state_dict()):
-    print(key)
-'''
-for i in range(10):
-    print(i)
+class Vgg19Ft(nn.Module):
+    def __init__(self, pretrained_model, num_classes):
+        super(Vgg19Ft, self).__init__()
+        self.features = pretrained_model.features
+        mod = list(pretrained_model.classifier.children())[:-1]
+        mod.append(nn.Linear(4096, num_classes))
+        self.classifier = nn.Sequential(*mod)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
